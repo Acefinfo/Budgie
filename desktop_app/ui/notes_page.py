b8bf852724d1243640f6117fa45156eea9c3ece3
Acefinfo <fromaashutoshthapa@gmail.com>
2025-10-28 09:29:11 +0545

    updated the functionality for overall use.

--- desktop_app/ui/notes_page.py
+++ desktop_app/ui/notes_page.py
@@ -1,17 +1,3 @@
-# from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QPushButton
-# from PySide6.QtCore import Signal
-
-# class NotesPage(QWidget):
-#     navigate_signal = Signal(str)
-
-#     def __init__(self):
-#         super().__init__()
-#         layout = QVBoxLayout()
-#         layout.addWidget(QLabel("📝 Notes Page"))
-#         btn_back = QPushButton("Back to Dashboard")
-#         btn_back.clicked.connect(lambda: self.navigate_signal.emit("dashboard"))
-#         layout.addWidget(btn_back)
-#         self.setLayout(layout)
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QTableWidget, QTableWidgetItem, QLineEdit, QMessageBox
 import requests
 from services import expense_api_service
